39f54ab1c73de01949fd3e9ac29a144300812548
washington <washinglopes@hotmail.com>
2023-10-05 15:40:03 -0300

    entrega de desafio

---
+++ src/Smartphone.java
@@ -0,0 +1,19 @@
+public class Smartphone {
+    public static void main(String[] args) throws Exception {
+        navegador();
+        musica();
+        ligar();      
+    }
+
+    public static void navegador() {
+        System.out.println("Você esta navegando");
+    }
+
+    public static void musica() {
+        System.out.println("Musica tocando");
+    }
+
+    public static void ligar() {
+        System.out.println("Fazendo ligação");
+    }
+}
